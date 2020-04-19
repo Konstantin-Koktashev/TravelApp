ef22b67264436fb81a45fcdbb4c94f8e8db7bc6d
Konstantin-Koktashev <lexxtalionis33@gmail.com>
2020-04-19 14:33:30 +0200

    Ver 0.4

--- javascript/modules/map-services.js
+++ javascript/modules/map-services.js
@@ -1,1 +1,14 @@
 'use stirct'
+const apiKey=AIzaSyBmx3Z42ngJl3kul0Ihag6WR2-P4SW2uuI
+window.addEventListener('load', onInit)
+
+function onInit(){
+    bindEvents()
+    
+}
+
+function bindEvents() {
+    document.querySelector('.go-to-location').addEventListener('click', onSearchLocation);
+    document.querySelector('.my-location').addEventListener('click', getMyLocation);
+    document.querySelector('.copy').addEventListener('click', onCopyLocationValue);
+}
