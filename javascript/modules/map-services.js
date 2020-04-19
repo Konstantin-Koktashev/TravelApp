7b98b25935bf03a850b52c73955aac0b3fe10074
Konstantin-Koktashev <lexxtalionis33@gmail.com>
2020-04-19 16:47:24 +0200

    Working Makrer

--- javascript/modules/map-services.js
+++ javascript/modules/map-services.js
@@ -7,12 +7,19 @@ export const mapServices={
     addNewMarker,
     addLocation
 }
-function initMap() {
+function initMap(positions) {
+    if(!positions) positions={lat:31.0461,lng:34.8516}
 var options={
     zoom:8,
-    center:{lat:42,lng:45}
+    center:positions
 }
 var map=new google.maps.Map(document.querySelector('.map-spot'),options);
+var marker = new google.maps.Marker({
+    position: positions,
+    map: map,
+    title: 'First Marker'
+  });
+  
 }
   
 function getMyLocaiton(){
@@ -20,11 +27,6 @@ function getMyLocaiton(){
 }
 
 function addNewMarker(){
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Hello World!'
-      });
     
 }
 
